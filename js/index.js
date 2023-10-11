60fb7f659ff86c0c414f5a20f275537f6a88a920
KillianFvt <killian.fauvette@gmail.com>
2023-10-11 23:39:16 +0200

    finished landing part + presentation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
 let strings = [
-  'Python Django',
-  'Python Flask',
-  'Dart Flutter',
-  'Html / Css / Javascript',
-  'C# .Net',
-  'C# .Net Core',
-  'C# .Net WPF',
+  'Python <img src=\"src/img/python.png\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'Python Django <img src=\"src/img/django.png\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'Python Flask <img src=\"src/img/flask.webp\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'Dart Flutter <img src=\"src/img/flutter.png\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'Html / Css / Javascript <img src=\"src/img/HCJ.png\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'C# .Net Core <img src=\"src/img/dotnet.webp\" style="display: inline; height: 2rem" alt="tech_icon">',
+  'C# .Net WPF <img src=\"src/img/dotnet.webp\" style="display: inline; height: 2rem" alt="tech_icon">',
 ]
 document.addEventListener('DOMContentLoaded', () => {
   let typed_q = new Typed('#typed_q', {
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     typeSpeed: 20,
     backSpeed: 30,
     backDelay: 1500,
-    loop: true
+    loop: true,
+    smartBackspace: true,
   });
 });
